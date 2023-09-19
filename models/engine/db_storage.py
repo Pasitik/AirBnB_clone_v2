e3ea6cabab070a725a99dad98348c130489a4ca5
Paul Asitik <paasitik@gmail.com>
2023-09-19 06:07:50 -0700

    =DB_storage Public instance methods

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,4 +1,6 @@
 from sqlalchemy import create_engine, session_maker
+import os
+
 
 Class DBStorage:
     __engine = None
@@ -6,6 +8,7 @@ Class DBStorage:
 
     def __init__(self):
         self.__engine = create_enginie('mysql+mysqldb://{}:{}@{}:3306/{}'.format("hbnb_dev", "hbnb_dev_pwd", "localhost", "hbnb_dev_db"), pool_pre_ping=True)
-        self.__session = session_maker(bind=engine) 
+        if os.env("HBNB_ENV") == "test":
+            Base.metadata.drop_all(self.__engine)
         
 
--- models/state.py
+++ models/state.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import Relationship 
 
 class State(BaseModel, Base):
     """ State class """
