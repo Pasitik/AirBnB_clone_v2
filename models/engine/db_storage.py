52ede464b5c88e583a9a3538264ce50cb854c4bd
Paul Asitik <paasitik@gmail.com>
2023-09-19 10:29:56 -0700

    DBStorage - States and Cities Update

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -11,4 +11,37 @@ Class DBStorage:
         if os.env("HBNB_ENV") == "test":
             Base.metadata.drop_all(self.__engine)
         
+    def all(self, cls=None):
+        if cls:
+            result = self.__session.query(cls).all()
+        else:
+            result = self.__session.query((State, City).outerjoin(City)).all()
+
+        query_result = {}
+        for obj in result:
+	        if cls is not None:
+                key = '{}.{}'.format(obj.__class__.__name__, obj.id)
+                query_result[key] = obj
+            else:
+                for row in obj:
+                    key = '{}.{}'.format(row.__class__.__name__, row.id)
+					query_result[key] = row
+
+        return query_result
+
+    def new(self, obj):
+        self.__session.add(obj)
+    def save(self):
+		self.__session.commit()
+	def delete(self, obj=None):
+        if obj is not None:
+            self.__session.delete(obj)
+
+    def reload(self):
+        from models import State, City, Amenity, Review, User, Place
+        
+		Base.metadata.create_all(engine)
+        session = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(session)
+		self.__session = Session()
 
