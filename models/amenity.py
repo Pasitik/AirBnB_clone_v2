56d8fbea64fcc904da3b5b00ec2cf6357081f793
Paul Asitik <paasitik@gmail.com>
2023-09-20 19:10:12 -0700

    DBStorage - Amenity... and BOOM!

--- models/amenity.py
+++ models/amenity.py
@@ -8,5 +8,6 @@ from sqlalchemy.orm import relationship
 class Amenity(BaseModel, Base):
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("
+    place_amenities = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary=place_amenity, back_populates="amenities")
     
