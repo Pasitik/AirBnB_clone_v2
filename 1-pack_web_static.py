4ca7a28048e9bde87900f333cd2abe57673387de
Paul Asitik <paasitik@gmail.com>
2023-10-05 00:22:49 -0700

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,18 +9,21 @@ from fabric.api import local, runs_once
 def do_pack():
     """Archives the static files."""
     dest = "versions"
+    script_directory = os.path.dirname(os.path.abspath(__file__))
     git_repo = "https://github.com/Pasitik/AirBnB_clone_v2.git"
     if not os.path.isdir(dest):
         os.makedirs(dest)
 
     local(f"git clone {git_repo}")
     repo_name = git_repo.split("/")[-1]
-    target = f"{repo_name}/web_static"
-    now = datatime.datetime.now()
+    new_name = repo_name.split(".")[-2]
+    target = f"{new_name}/web_static"
+    now = datetime.datetime.now()
+    archive_name = f"web_static_{now.strftime('%Y%m%d%H%M%S')}.tgz"
     try:
-        local(f"tar -czf {dest}/web_static_{now.strftime('%Y%m%d%H%M%S')}.tgz -C {target} .")
-        local(f"rm -rf {repo_name}")
-        return os.path.join(destination_directory, archive_name)
+        local(f"tar -czf {dest}/{archive_name} -C {target} .")
+        local(f"rm -rf {new_name}")
+        return os.path.join(dest, archive_name)
     except Exception as e:
         print(f"Error creating archive: {e}")
         return None
